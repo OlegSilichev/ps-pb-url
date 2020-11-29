7d1cdd96c4f65cfcb33af29821b76efb69cfe553
OlegSilichev <olsi-86@mail.ru>
2020-11-30 00:22:32 +0700

    Version2

--- ps-pb-url.py
+++ ps-pb-url.py
@@ -2,14 +2,13 @@ import os
 
 os.system('cls')
 
-def get_absolute_url(domen,*args,**kwargs):
-    result = f'{domen}'
+def get_absolute_url(url,*args,**kwargs):
     for i in args:
-        result = f'{result}/{i}'
-    result = f'{result}?'
+        url = f'{url}/{i}'
+    url = f'{url}?'
     for k,v in kwargs.items():
-        result = f'{result}{k}={v}&'
-    return result[:-1] 
+        url = f'{url}{k}={v}&'
+    return url[:-1] 
     
 print(get_absolute_url('www.yandex.ru', 'posts', 'news', id='24',author='admin'))
 print(get_absolute_url('www.google.com', 'images', id='24', category='auto', color='red', size='small'))
